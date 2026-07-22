754a52aab711bbce530e52318a955282bfe37006
ilyav <ilyav@ILIA>
2026-07-22 22:50:46 +0500

    Colors DONE

---
+++ JavaScript/forms_and_events.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+
+function Factorial() {
+	let numberElement = document.getElementById("number");
+	let number = numberElement.value;
+	let resultElement = document.getElementById("factorial-result");
+	let f = BigInt(1);
+	for (let i = 1n; i <= number; i++) {
+		f *= i;
+	}
+	//resultElement.value = `${number}! = ${f}`;
+	resultElement.innerHTML = `${number}! = ${f}`;
+}
+
+function setImage() {
+	let filename = document.getElementById("image-file");
+	let reader = new FileReader();
+	reader.onload = function (e) {
+		document.getElementById("image").src = e.target.result;
+	}
+	reader.readAsDataURL(filename.files[0]);
+}
+function setBackgroundColor()
+{
+	document.body.style.backgroundColor = document.getElementById("background-color").value;
+}
+function setForegroundColor()
+{
+	document.body.style.color = document.getElementById("foreground-color").value;
+}
